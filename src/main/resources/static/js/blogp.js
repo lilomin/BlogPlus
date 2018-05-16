fb6cf1f71fca3de2ceb65b14f48b48091c8e57f2
lilomin <lilomin@126.com>
2018-05-16 20:01:37 +0800

    blog title

--- src/main/resources/static/js/blogp.js
+++ src/main/resources/static/js/blogp.js
@@ -24,7 +24,8 @@ app.loadHomeCard = function (data) {
         var tags = app.generateTags(blog.tags);
         
         var card = '<div class="card"><img class="card-img-top" src="/api/v1/file/' + blog.image + '" alt="Card image cap">' +
-                '<div class="card-body"><h5 class="card-title">' + blog.title + '</h5>' + '<div class="card-tag">' + tags + '</div>' +
+                '<div class="card-body"><a href="post' + blog.path + '"><h5 class="card-title">' + blog.title + '</h5></a>' +
+                '<div class="card-tag">' + tags + '</div>' +
                 '<p class="card-text card-desc">' + blog.description + '</p>' + 
                 '<p class="card-text">' + 
                 '<small class="text-muted">' + blog.createDay + '</small><a href="post/' + blog.path + '" ' +
